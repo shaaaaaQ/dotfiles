6dbd06146e2adf16f42c2811009ee867aa54eeae
sh <64454953+shaaaaaQ@users.noreply.github.com>
2023-01-21 18:16:17 +0900

    dunst

--- link.sh
+++ link.sh
@@ -6,8 +6,12 @@ rm -rf ~/.config/polybar
 rm -rf ~/.config/rofi
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/picom
+rm -rf ~/.config/dunst
+
 rm -rf ~/.bin
 rm -rf ~/.wallpaper
+
+
 
 ln -nfs ~/dotfiles/config/nvim ~/.config/nvim
 ln -nfs ~/dotfiles/config/i3 ~/.config/i3
@@ -15,7 +19,10 @@ ln -nfs ~/dotfiles/config/polybar ~/.config/polybar
 ln -nfs ~/dotfiles/config/rofi ~/.config/rofi
 ln -nfs ~/dotfiles/config/alacritty ~/.config/alacritty
 ln -nfs ~/dotfiles/config/picom ~/.config/picom
+ln -nfs ~/dotfiles/config/dunst ~/.config/dunst
+
 ln -nfs ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
 ln -nfs ~/dotfiles/config/fcitx5/conf/classicui.conf ~/.config/fcitx5/conf/classicui.conf
+
 ln -nfs ~/dotfiles/bin ~/.bin
 ln -nfs ~/dotfiles/wallpaper ~/.wallpaper
