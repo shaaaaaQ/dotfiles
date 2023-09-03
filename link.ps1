8bb5449b895bdef25f4a5c95dbf75e00b6446b2d
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-09-04 00:04:47 +0900

    windows wakaran

--- link.ps1
+++ link.ps1
@@ -2,8 +2,10 @@ Remove-Alias -Name rm
 
 rm -rf $env:USERPROFILE\AppData\Local\nvim
 rm -rf $env:USERPROFILE\AppData\Roaming\alacritty
-rm -rf $env:USERPROFILE\Documents\PowerShell
+rm -rf $env:USERPROFILE\AppData\Roaming\CorvusSKK
+rm -rf "$([Environment]::GetFolderPath("MyDocuments"))\PowerShell"
 
 ln -nfs $env:USERPROFILE\dotfiles\config\nvim $env:USERPROFILE\AppData\Local\nvim
 ln -nfs $env:USERPROFILE\dotfiles\windows\alacritty $env:USERPROFILE\AppData\Roaming\alacritty
-ln -nfs $env:USERPROFILE\dotfiles\windows\PowerShell $env:USERPROFILE\Documents\PowerShell
+ln -nfs $env:USERPROFILE\dotfiles\windows\CorvusSKK $env:USERPROFILE\AppData\Roaming\CorvusSKK
+ln -nfs $env:USERPROFILE\dotfiles\windows\PowerShell "$([Environment]::GetFolderPath("MyDocuments"))\PowerShell"
