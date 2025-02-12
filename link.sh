f176eb89e3a12df9a0ff83fbcf8e09843eba7f9f
sh <64454953+shaaaaaQ@users.noreply.github.com>
2025-02-12 21:12:40 +0900

    waybar

--- link.sh
+++ link.sh
@@ -14,6 +14,7 @@ rm -rf ~/.config/Thunar
 rm -rf ~/.config/libskk
 rm -rf ~/.config/hypr
 rm -rf ~/.config/niri
+rm -rf ~/.config/waybar
 
 rm -rf ~/.bin
 rm -rf ~/.wallpaper
@@ -31,6 +32,7 @@ ln -nfs ~/dotfiles/config/Thunar ~/.config/Thunar
 ln -nfs ~/dotfiles/config/libskk ~/.config/libskk
 ln -nfs ~/dotfiles/config/hypr ~/.config/hypr
 ln -nfs ~/dotfiles/config/niri ~/.config/niri
+ln -nfs ~/dotfiles/config/waybar ~/.config/waybar
 
 ln -nfs ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
 ln -nfs ~/dotfiles/config/fish/fish_plugins ~/.config/fish/fish_plugins
