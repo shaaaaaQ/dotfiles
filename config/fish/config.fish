e060c877995a84a2d19e0954f88941d7a1d9b352
sh <64454953+shaaaaaQ@users.noreply.github.com>
2026-05-18 20:16:44 +0900

    pnpm

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,15 @@ alias :q='exit'
 
 pyenv init --path | source
 pyenv init - | source
+
+nvm use latest --silent
+
+alias npm="echo 'npm is disabled. Please use pnpm instead.' && false"
+alias npx="echo 'npx is disabled. Please use pnpm dlx instead.' && false"
+
+# pnpm
+set -gx PNPM_HOME "/home/sh/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
